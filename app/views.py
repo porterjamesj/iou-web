70ed97e75b9adbdb7e8bda9c365dd050d5cf11eb
James Porter <porterjamesj@gmail.com>
2013-07-03 13:27:04 -0400

    Add more sophisticated login/dashboard, make homepage functional.

--- app/views.py
+++ app/views.py
@@ -14,13 +14,6 @@ def load_user(id):
 def index():
     return render_template('index.html')
 
-# the dashboard view
-
-@app.route('/dash/')
-@login_required
-def dashboard():
-    return str(current_user.id)
-
 # logging in and out
 
 @app.route('/login', methods = ['GET','POST'])
@@ -30,20 +23,35 @@ def login():
         # try to log the user in
         user = User.query.filter_by(email = form.email.data).first()
         if user == None:
+            # the user can't be found
             flash("No such user.")
             return render_template('login.html',form=form)
         elif not user.check_password(form.password.data):
+            # the password is wrong
             flash("Password incorrect.")
             return render_template('login.html',form=form)
         else:
+            # login was successful
             login_user(user)
-            return redirect(request.args.get('next')) or \
-                   redirect(url_for('dashboard'))
+            if request.args.get('next'):
+                # redirect if the user came from somewhere
+                return redirect(request.args.get('next'))
+            else:
+                # otherwise just show the dashboard
+                return redirect(url_for('dashboard'))
     else:
         return render_template('login.html',form = form)
 
-@app.route("/logout")
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect('index')
+    return redirect("/index")
+
+# the dashboard view
+
+@app.route('/dash')
+@login_required
+def dashboard():
+    return render_template('dash.html',user=current_user,
+                           toolbar = True)
