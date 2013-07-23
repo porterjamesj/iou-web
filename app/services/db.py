9432ed9dc194da60c4e9e57e7854a77fee52a1e9
James Porter <porterjamesj@gmail.com>
2013-07-23 14:25:09 -0400

    More pep8.

--- app/services/db.py
+++ app/services/db.py
@@ -1,17 +1,19 @@
-from app.models import Trans, User, Member, Group, CLEAR_ALL
+from app.models import Trans, User, Member, Group
 from app import db
 from time import time
 
+
 def add_transaction(group_id, from_id, to_id, amount, kind):
     """Add the specified transaction to the database."""
-    newtrans = Trans(group_id = group_id,
-                     from_id = from_id,
-                     to_id = to_id,
-                     amount = amount,
-                     time = time(),
-                     kind = kind)
+    newtrans = Trans(group_id=group_id,
+                     from_id=from_id,
+                     to_id=to_id,
+                     amount=amount,
+                     time=time(),
+                     kind=kind)
     db.session.add(newtrans)
     db.session.commit()
+
 
 def users_exist(user_ids):
     """Given a list of user ids, return a list of them if they do exist,
@@ -22,15 +24,17 @@ def users_exist(user_ids):
     else:
         return users
 
+
 def group_exists(group_id):
     """Return the group if it exists, otherwise return False."""
     group = Group.query.get(group_id)
-    if group == None:
+    if group is None:
         return False
     else:
         return group
 
-def users_in_group(users,group):
+
+def users_in_group(users, group):
     """Return true if all users are members of the group, false
     otherwise."""
     if all([user in group.members for user in users]):
@@ -38,17 +42,19 @@ def users_in_group(users,group):
     else:
         return False
 
-def user_is_admin(user,group):
+
+def user_is_admin(user, group):
     """Return true if the user is an admin for the group, false
     otherwise."""
-    admins = Member.query.filter(Member.admin == True,
+    admins = Member.query.filter(Member.admin is True,
                                  Member.group_id == group.id).all()
     if user.id in [admin.user_id for admin in admins]:
         return True
     else:
         return False
 
-def set_admins(users,group,setting):
+
+def set_admins(users, group, setting):
     """Set the admin status of the user with user_id in the group
     group_id to the requested setting."""
     members = Member.query.filter(Member.user_id.in_([u.id for u in users]),
@@ -57,20 +63,26 @@ def set_admins(users,group,setting):
         member.admin = setting
     db.session.commit()
 
+
 def search_users(querystring):
     """Return all the users whose name or email is like the query string."""
-    foundname = User.query.filter(User.name.like("%" + querystring + "%")).all()
-    foundemail = User.query.filter(User.email.like("%" + querystring + "%")).all()
+    foundname = User.query.filter(User.name.like("%" +
+                                                 querystring + "%")).all()
+    foundemail = User.query.filter(User.email.like("%" +
+                                                   querystring + "%")).all()
     return set(foundname+foundemail)
+
 
 def search_groups(querystring):
     """Return all groups whose name is like the query string."""
-    return set(Group.query.filter(Group.name.like("%" + querystring + "%")).all())
+    return set(Group.query.filter(Group.name.like("%" +
+                                                  querystring + "%")).all())
 
-def add_member(user,group):
+
+def add_member(user, group):
     """Make the user a member of the group, but not an admin."""
-    member = Member(user_id = user.id,
-                    group_id = group.id,
-                    admin = False)
+    member = Member(user_id=user.id,
+                    group_id=group.id,
+                    admin=False)
     db.session.add(member)
     db.session.commit()
--- app/services/graph.py
+++ app/services/graph.py
@@ -7,7 +7,8 @@ from app.models import CLEAR_ALL
 #These functions work on graphs, which are dicts of dicts that can be thought
 #of as graph[creditor][debtor] = amount.
 
-def build_graph(users,transactions):
+
+def build_graph(users, transactions):
     """Given lists of users and transactions, return the corresponding
     graph. Transactions must be ordered by time for this to work correctly."""
     # check that the transactions are all for the one group
@@ -24,15 +25,16 @@ def build_graph(users,transactions):
         if trans.kind == CLEAR_ALL:
             graph = deepcopy(emptygraph)
         else:
-            graph[trans.to_id][trans.from_id] += float(trans.amount) # from Decimal
+            graph[trans.to_id][trans.from_id] += float(trans.amount)
     # simplify graph
     return simplify(graph)
+
 
 def get_flows(graph):
     """Given a graph, return a set of (name, flow) tuples indicating the flow
     in or out of each person in the graph. Flow is positive for a net creditor
     and negative for a net debtor. """
-    nodeflows = {person : 0.0 for person in graph}
+    nodeflows = {person: 0.0 for person in graph}
     for person1 in graph:
         for person2 in graph[person1]:
             value = graph[person1][person2]
@@ -40,11 +42,12 @@ def get_flows(graph):
             nodeflows[person2] -= value
     return set(nodeflows.items())
 
-def add(graph,creditor,debtors,amount):
+
+def add(graph, creditor, debtors, amount):
     """Return a new graph with the specified debt added to it. Debtors should
     be either a key in the dict or a list of keys."""
-    newgraph = deepcopy(graph) # the graph we will return
-    if isinstance(debtors,list):
+    newgraph = deepcopy(graph)  # the graph we will return
+    if isinstance(debtors, list):
         # check that all the debtors actually exist
         for debtor in debtors:
             if debtor not in newgraph.keys():
@@ -58,7 +61,7 @@ def add(graph,creditor,debtors,amount):
                 # group that includes them (and therefore is both a
                 # debtor and the creditor) work correctly.
                 newgraph[creditor][debtor] += each_amount
-    else: # the debtor is a key in the graph
+    else:  # the debtor is a key in the graph
         # check that the debtor exists
         if debtors not in newgraph.keys():
             raise KeyError("debtor not found.")
@@ -66,15 +69,16 @@ def add(graph,creditor,debtors,amount):
         newgraph[creditor][debtors] += float(amount)
     return newgraph
 
+
 def flows2graph(flows):
-    """Given a set of n flows for a connected graph, construct the simplest
-    possible graph that satisfies their values. (this graph will have n-1 edges)
-    """
+    """Given a set of n flows for a connected graph, construct the
+    simplest possible graph that satisfies their values. (this graph
+    will have n-1 edges)"""
     # Split flows into positive and negative and sort each
     pos_flows = [list(flow) for flow in flows if flow[1] > 0]
     neg_flows = [list(flow) for flow in flows if flow[1] < 0]
-    pos_flows.sort(key = lambda t: -t[1])
-    neg_flows.sort(key = lambda t: t[1])
+    pos_flows.sort(key=lambda t: -t[1])
+    neg_flows.sort(key=lambda t: t[1])
     # Now build the new optimal debt graph
     graph = {flow[0]: defaultdict(float) for flow in flows}
     while pos_flows and neg_flows:
@@ -97,22 +101,24 @@ def flows2graph(flows):
             pos_flows[0][1] += debit
     return graph
 
-def partition(flows,n=2):
+
+def partition(flows, n=2):
     """Given a set of flows, return the longest list of zero-sum
     subsets that covers the original set. Can specify the minimum
     size of a subset with `n`."""
-    for subset in combinations(flows,n):
+    for subset in combinations(flows, n):
         if sum(flow[1] for flow in subset) == 0.0:
             # this is a zero-sum node
             # recurse
             subset = set(subset)
-            return [subset] + partition(flows.difference(subset),n)
-    # if there are no zero-sum subsets, first check if we have found all subsets
+            return [subset] + partition(flows.difference(subset), n)
+    # if there are no zero-sum subsets, first check if we have found
+    # all subsets
     if n >= len(flows)//2:
         # there are no more left to find
         return [flows]
     else:
-        return partition(flows,n+1)
+        return partition(flows, n+1)
 
 
 def simplify(graph):
@@ -129,13 +135,14 @@ def simplify(graph):
     # once we have the subsets, build the simplest possible graph for each
     newgraphs = [flows2graph(subset) for subset in subsets]
     # merge all the constructed graphs together into one dict
-    finalgraph = { k: v for graph in newgraphs for k, v in graph.items() }
+    finalgraph = {k: v for graph in newgraphs for k, v in graph.items()}
     # append all the zero nodes
     for node in zeroflows:
         finalgraph[node[0]] = {}
     return finalgraph
 
-def display_graph(users,graph):
+
+def display_graph(users, graph):
     """Converts a graph in terms of user ids to one in terms of usernames,
     suitable for display. Requires a list of users to use for the mapping."""
     dispgraph = {}
