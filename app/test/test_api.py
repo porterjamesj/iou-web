c655e664819641d1d94e11fc7117750c68db07d2
James Porter <porterjamesj@gmail.com>
2013-07-27 18:28:57 -0400

    Rewrite API tests for new API.

--- app/test/test_api.py
+++ app/test/test_api.py
@@ -1,23 +1,35 @@
-from flask import json, request
+from flask import json
 from flask.ext.testing import TestCase
-from app.models import User,Group,Member,Trans
+from flask.ext.login import LoginManager
+from app.models import User, Group
 from app import app, api
+from app import errors as err
 from mock import Mock
+from nose.tools import raises
+from werkzeug.exceptions import NotFound
+
 
 class APIBase(TestCase):
-    """Base class for all api tests to inherit from."""
     def create_app(self):
-        app.config['LOGIN'] = False
+        app.config['TESTING'] = True
+        login_manager = LoginManager()
+        login_manager.init_app(app)  # this has to happen to turn login off
         return app
 
 
-class TestAddTrans(APIBase):
+class ParsesJson():
+    @raises(err.JSONParseError)
+    def test_fail_if_bad_json(self):
+        with app.test_request_context(data="garbage"):
+            api.post_trans(dbsrv=self.sm)
+
+
+class TestPostTrans(APIBase, ParsesJson):
     @classmethod
     def setup_class(self):
-        """Mock services layer, and request."""
-        self.srvmock = Mock()
+        self.sm = Mock()  # mock for the database services layer
         data = json.dumps({"group_id": 1,
-                           "from": [1],
+                           "from_ids": [1],
                            "to_id": [2],
                            "amount": 20,
                            "kind": "debt"})
@@ -26,183 +38,145 @@ class TestAddTrans(APIBase):
                                                      "application/json")
 
     def setUp(self):
-        """Reset the mock values for each test."""
-        self.srvmock.users_exist.return_value = [User(), User()]
-        self.srvmock.group_exists.return_value = Group()
-        self.srvmock.users_in_group.return_value = True
-        self.srvmock.add_transaction.return_value = None
+        self.sm.get_users.return_value = [User(), User()]
+        self.sm.get_groups.return_value = Group()
+        self.sm.users_in_group.return_value = True
+        self.sm.add_transaction.return_value = None
 
     def test_succeed_normally(self):
         with self.fake_request:
-            print api.addtrans(dbsrv=self.srvmock).get_data()
-            assert "0" in api.addtrans(dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_user_does_not_exist(self):
-        with self.fake_request:
-            self.srvmock.users_exist.return_value = False
-            assert "1" in api.addtrans(dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_group_does_not_exist(self):
-        with self.fake_request:
-            self.srvmock.group_exists.return_value = False
-            assert "2" in api.addtrans(dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_user_is_not_in_group(self):
-        with self.fake_request:
-            self.srvmock.users_in_group.return_value = False
-            assert "3" in api.addtrans(dbsrv=self.srvmock).get_data()
+            self.assertStatus(api.post_trans(dbsrv=self.sm), 200)
 
 
-class TestClearAll(APIBase):
-
+class TestSearchUsers(APIBase, ParsesJson):
     @classmethod
     def setup_class(self):
-        """Mock services layer, and request."""
-        self.srvmock = Mock()
-        self.fake_request = app.test_request_context()
-
-    def setUp(self):
-        self.srvmock.group_exists.return_value = Group()
-        self.srvmock.user_is_admin.return_value = True
-
-    def test_succeed_normally(self):
-        with self.fake_request:
-            assert "0" in api.clearall(1, dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_group_does_not_exist(self):
-        with self.fake_request:
-            self.srvmock.group_exists.return_value = False
-            assert "2" in api.clearall(1, dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_current_user_is_not_admin(self):
-        with self.fake_request:
-            self.srvmock.user_is_admin.return_value = False
-            assert "4" in api.clearall(1, dbsrv=self.srvmock).get_data()
-
-
-class TestAddAdmin(APIBase):
-    @classmethod
-    def setup_class(self):
-        """Mock services layer, and request."""
-        self.srvmock = Mock()
-        data = json.dumps({"group": 1,
-                           "user": [1]})
+        self.sm = Mock()  # mock for the database services layer
+        data = json.dumps({"query": "j"})
         self.fake_request = app.test_request_context(data=data,
                                                      content_type=
                                                      "application/json")
 
     def setUp(self):
-        self.srvmock.group_exists.return_value = Group()
-        self.srvmock.users_exist.return_value = [User()]
-        self.srvmock.users_in_group.return_value = True
-        self.srvmock.user_is_admin.return_value = False
+        self.sm.search_users.return_value = [User()]
 
     def test_succeed_normally(self):
         with self.fake_request:
-            assert "0" in api.addadmin(dbsrv=self.srvmock).get_data()
+            self.assertStatus(api.search_users(dbsrv=self.sm), 200)
 
-    def test_fail_if_user_does_not_exist(self):
-        with self.fake_request:
-            self.srvmock.users_exist.return_value = False
-            assert "1" in api.addadmin(dbsrv=self.srvmock).get_data()
 
-    def test_fail_if_group_does_not_exist(self):
-        with self.fake_request:
-            self.srvmock.group_exists.return_value = False
-            assert "2" in api.addadmin(dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_user_is_admin(self):
-        with self.fake_request:
-            self.srvmock.user_is_admin.return_value = True
-            assert "5" in api.addadmin(dbsrv=self.srvmock).get_data()
-
-class TestResign(APIBase):
+class TestGetUser(APIBase):
     @classmethod
     def setup_class(self):
-        """Mock services layer, and request."""
-        self.srvmock = Mock()
+        self.sm = Mock()  # mock for the database services layer
         self.fake_request = app.test_request_context()
-
-    def setUp(self):
-        self.srvmock.group_exists.return_value = True
-        self.srvmock.users_in_group.return_value = True
-        self.srvmock.user_is_admin.return_value = True
 
     def test_succeed_normally(self):
+        self.sm.get_user.return_value = User()
         with self.fake_request:
-            assert "0" in api.resign(1, dbsrv=self.srvmock).get_data()
+            self.assertStatus(api.get_user(1, dbsrv=self.sm), 200)
 
-    def test_fail_if_group_does_not_exist(self):
+    @raises(NotFound)
+    def test_404_if_no_such_user(self):
+        self.sm.get_user.return_value = None
         with self.fake_request:
-            self.srvmock.group_exists.return_value = False
-            assert "2" in api.resign(1, dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_user_not_in_group(self):
-        with self.fake_request:
-            self.srvmock.users_in_group.return_value = False
-            assert "3" in api.resign(1, dbsrv=self.srvmock).get_data()
-
-    def test_fail_if_user_not_admin(self):
-        with self.fake_request:
-            self.srvmock.user_is_admin.return_value = False
-            assert "4" in api.resign(1, dbsrv=self.srvmock).get_data()
+            api.get_user(1, dbsrv=self.sm)
 
 
-class TestSearchUsers(APIBase):
+class TestPutUser(APIBase, ParsesJson):
     @classmethod
     def setup_class(self):
-        self.srvmock = Mock()
-        self.srvmock.search_users.return_value = [User(name="james", id=3,
-                                                       email=
-                                                       "james@gmail.com")]
-        self.fake_request = app.test_request_context()
+        self.sm = Mock()
+        data = json.dumps({"name": "test"})
+        self.fake_request = app.test_request_context(data=data,
+                                                     content_type=
+                                                     "application/json")
 
-    def test_jsonifys_users(self):
+    def test_succeed_normally(self):
+        self.sm.change_user.return_value = None
         with self.fake_request:
-            assert "james@gmail" in api.search_users("james",
-                                                     dbsrv=self.srvmock)\
-                                       .get_data()
-            assert "3" in api.search_users("james",
-                                           dbsrv=self.srvmock).get_data()
+            self.assertStatus(api.put_user(1, dbsrv=self.sm), 200)
 
 
-class TestSearchGroups(APIBase):
+class TestSearchGroups(APIBase, ParsesJson):
     @classmethod
     def setup_class(self):
-        self.srvmock = Mock()
-        self.srvmock.search_groups.return_value = [Group(id=3,
-                                                         name="Soccer Club")]
-        self.fake_request = app.test_request_context()
-
-    def test_jsonifys_groups(self):
-        with self.fake_request:
-            assert "Club" in api.search_groups("soccer",
-                                               dbsrv=self.srvmock).get_data()
-            assert "3" in api.search_groups("soccer",
-                                            dbsrv=self.srvmock).get_data()
-
-
-class TestAddMember(APIBase):
-
-    @classmethod
-    def setup_class(self):
-        self.srvmock = Mock()
-        data = json.dumps({"group_id": 1,
-                           "user_id": 1})
+        self.sm = Mock()
+        data = json.dumps({"query": "test"})
         self.fake_request = app.test_request_context(data=data,
                                                      content_type=
                                                      "application/json")
 
     def setUp(self):
-        self.srvmock.users_exist.return_value = [User()]
-        self.srvmock.group_exists.return_value = True
-        self.srvmock.users_in_group.return_value = False
+        self.sm.search_groups.return_value = [Group()]
 
     def test_succeed_normally(self):
         with self.fake_request:
-            assert "0" in api.add_member(dbsrv=self.srvmock).get_data()
+            self.assertStatus(api.search_groups(dbsrv=self.sm), 200)
 
-    def test_fail_if_user_does_not_exist(self):
-        self.srvmock.users_exist.return_value = False
+
+class TestGetGroup(APIBase):
+    @classmethod
+    def setup_class(self):
+        self.sm = Mock()
+
+    def test_succeed_normally(self):
+        self.sm.get_group.return_value = Group()
+        self.assertStatus(api.get_group(1, dbsrv=self.sm), 200)
+
+    @raises(NotFound)
+    def test_fail_if_group_not_found(self):
+        self.sm.get_group.return_value = None
+        api.get_group(1, dbsrv=self.sm)
+
+
+class TestPutGroup(APIBase, ParsesJson):
+    @classmethod
+    def setup_class(self):
+        self.sm = Mock()
+        data = json.dumps({"name": "test"})
+        self.fake_request = app.test_request_context(data=data,
+                                                     content_type=
+                                                     "application/json")
+
+    def test_succeed_normally(self):
         with self.fake_request:
-            assert "1" in api.add_member(dbsrv=self.srvmock).get_data()
+            self.assertStatus(api.put_group(1, dbsrv=self.sm), 200)
+
+
+class TestIsMember(APIBase):
+    @classmethod
+    def setup_class(self):
+        self.sm = Mock()
+
+    def test_succeed_normally(self):
+        self.sm.users_in_group.return_value = True
+        self.assertStatus(api.is_member(1, 1, dbsrv=self.sm), 200)
+
+    @raises(NotFound)
+    def test_404_if_no_such_member(self):
+        self.sm.users_in_group.return_value = False
+        api.is_member(1, 1, dbsrv=self.sm)
+
+
+class TestNewMember(APIBase):
+    @classmethod
+    def setup_class(self):
+        self.sm = Mock()
+
+    def test_succeed_normally(self):
+        self.assertStatus(api.new_member(1, 1, dbsrv=self.sm), 200)
+
+
+class TestPutSelf(APIBase, ParsesJson):
+    @classmethod
+    def setup_class(self):
+        self.sm = Mock()
+        data = json.dumps({"action": "resign"})
+        self.fake_request = app.test_request_context(data=data,
+                                                     content_type=
+                                                     "application/json")
+
+    def test_succeed_normally(self):
+        with self.fake_request:
+            self.assertStatus(api.put_self(1, dbsrv=self.sm), 200)
